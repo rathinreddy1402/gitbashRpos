5a10f4ba7fc036b6f5622538aca1468684f07533
rathinreddy1402 <rathinreddy1402@gmail.com>
2023-03-28 06:31:57 +0530

    1st commit

---
+++ src/test/java/Pratice/Sample.java
@@ -0,0 +1,21 @@
+package Pratice;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+	public void test()
+	{
+		System.out.println("method running");
+	}
+}
+//mvn clean
+//mvn validate
+//mvn compile
+//mvn test
+//mvn -Dtest=classname test
+
+//pull back
+
+//
